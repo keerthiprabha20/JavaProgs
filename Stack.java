7e8b8e44e7c8531962672e0bcc084fdf8cfbcbf6
keerthiprabha20 <keerthip152@gmail.com>
2025-09-19 00:18:59 +0530

    Add files via upload

--- Stack.java
+++ Stack.java
@@ -1,87 +1,113 @@
-package stackperson;
+package library;
 
 public class Stack {
-	Person arr[];
-	int top; 
-	int element;
-	
-	Stack(int size)
-	{
-		element=size;
-		arr=new Person[element];
-		top=-1;
-	}
-	
-   Stack(Person[] inputArr)
-   {
-	   element= inputArr.length;
-	   arr=new Person[element];
-	   top=-1;
-	   for (int i=0; i<inputArr.length;i++ ) {
-		   push (inputArr[i]);
-	   }
-   }
-   
-   void push(Person p) {
-	   if (top>=element-1){
-		   System.out.println("Stack overflow"+p);
-		   return;
-	   }
-	   arr[++top]=p;
-   }
-   void push(Person p1, Person p2) {
-	   push(p1);
-	   push(p2);
-	   }
-   
-Person pop()
-{
-	if(top==-1) {
-		System.out.println("stack underflow ");
-		return null;
-	}
-	return arr[top--];
-	}
-void pop(int n) {
-	if(n>top+1) {
-		System.out.println("stack empty");
-		n=top+1;
-	}
-	for (int i=0;i< n;i++) {
-		Person p=pop();
-		if(p!=null) {
-			System.out.println("popped: ");
-			p.displayPerson();
-		}
-	}
-}
-
-void display()
-{
-	if(top==-1) {
-		System.out.println("Stack is empty");
-		return;
-	}
-	System.out.println("stack from top to  bottom");
-	for(int i=top; i>=0; i--)
-	{
-		arr[i].displayPerson();
-	}
-}
-void display(int n)
-{
-	if (n>top+1) {
-		System.out.println("only"+(top+ 1)+"person in stack");
-		n=top+1;
-		
-	}
-	System.out.println("top "+n+" persons");
-	for(int i=top; i>top-n;i--) {
-		arr[i].displayPerson();
-	}
-}
-}
-		
-	
-
-
+		      private int arr[];  
+			  private int top; 
+			  private int capacity;  
+			  
+			      Stack(int size)   
+			      {  
+			          capacity = size;   
+			          arr = new int[capacity];   
+			          top = -1;   
+			      }  
+			  
+			       
+			      Stack(int[] inputArr)  
+			      {  
+			       capacity = inputArr.length;   
+			          arr = new int[capacity];   
+			          for (int i = 0; i < capacity; i++)  
+			          {   
+			              arr[i] = inputArr[i];   
+			          }   
+			          top = capacity - 1;   
+			      }  
+			  
+			        
+			      void push(int x)  
+			      {  
+			         if (top >= capacity - 1)  
+			         {   
+			                System.out.println("Stack Overflow" + x);   
+			                return;   
+			            }   
+			   
+			            arr[++top] = x;   
+			            System.out.println("Pushed: " + x);   
+			      }  
+			  
+			        
+			      void push(int x, int y)  
+			      {  
+			        push(x);   
+			           push(y);   
+			      }  
+			  
+			        
+			      int pop()  
+			      {  
+			          if (top == -1)  
+			          {   
+			               System.out.println("Stack Underflow");   
+			               return -1;   
+			           }   
+			           return arr[top--];    
+			      }  
+			  
+			        
+			      void pop(int n)   
+			      {  
+			       if (n <= 0)   
+			       {   
+			                     System.out.println("Invalid number of elements to pop");   
+			                    return;   
+			          }   
+			     
+			          for (int i = 0; i < n; i++)  
+			          {   
+			              int val = pop();   
+			              if (val == -1) break;   
+			              System.out.println("Popped: " + val);   
+			          }     
+			      }  
+			  
+			        
+			      void display()  
+			      {  
+			       if (top == -1)  
+			       {   
+			                     System.out.println("Stack is empty");   
+			                    return;  
+			                  }  
+			        System.out.println("Stack contents (Top to Bottom):");   
+			                  for (int i = top; i >= 0; i--)  
+			                  {   
+			                     System.out.println(arr[i]);   
+			                }   
+			      }  
+			  
+			        
+			      void display(int n)  
+			      {  
+			  
+			        if (top == -1)  
+			   
+			        {   
+			                       System.out.println("Stack is empty");   
+			                       return;   
+			           }   
+			      
+			           if (n <= 0)   
+			           {   
+			               System.out.println("Invalid number of elements");   
+			               return;   
+			           }   
+			      
+			           System.out.println("Top " + n + " elements:");   
+			           for (int i = top; i >= 0 && i > top - n; i--)  
+			           {   
+			               System.out.println(arr[i]);   
+			           }   
+			       }   
+              }
